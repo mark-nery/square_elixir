5d0121292a17b826c38e0bddf1eb869d61fa373d
mark nery <marknery@gmail.com>
2017-05-16 17:40:34 -0600

    updated metadata

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule SquareElixir.Mixfile do
   defp package do
     [
       name: :postgrex,
-      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      files: ["lib", "mix.exs", "README*"],
       maintainers: ["Mark Nery"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/mark-nery/square_elixir"}
